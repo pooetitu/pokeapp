33165b998653f69dd641453cc7f071881d3876c0
pooetitu <pooetitu@gmail.com>
2020-09-23 11:13:36 +0200

    Added attributes to player object Added game module

--- api/index.js
+++ api/index.js
@@ -1,5 +1,6 @@
 import http from 'http';
 import socketIO from 'socket.io'
+import { startGame, terminateGame, handleMove } from './game';
 
 
 const players = [];
@@ -9,7 +10,7 @@ const server = http.createServer((req, res) => {
     res.write('Hello world');
     res.end();
 });
-
+ 
 const port = 3000;
 server.listen(port, () => {
     console.log('server listenning');
@@ -34,10 +35,13 @@ function addNewPlayer(socket){
         let number = Math.floor(Math.random() * Math.floor(99999));
         name += '-' + number;
         console.log(name + ' is connected');
-        players.push(name);
+        players.push({name: name, socket: socket, pokemon: null});
+        if(players.length === 2){
+            startGame(players);
+        }
     }
     else{
-        socket.emit('Too many players connected');
+        socket.emit('connected', 'Too many players connected');
         socket.disconnect();
     }
 }
