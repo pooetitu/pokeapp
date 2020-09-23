fa56f31dd0ad71d2a78845d99089ed5cb1c05b8d
pooetitu <pooetitu@gmail.com>
2020-09-23 14:48:59 +0200

    Correction

--- api/game.js
+++ api/game.js
@@ -7,30 +7,29 @@ export const startGame = (players, config) => {
     for (let player of players){
         let rndPokemon = Math.floor(Math.random() * pokemons.length);
         console.log(pokemons[rndPokemon].name + ' Assigned to ' + player['name']);
-        player['pokemon'] = pokemons[rndPokemon];
+        player['pokemon'] = {...pokemons[rndPokemon]};
     }
-    for (let i=0;i<players.length;i++) {
-
-        let you = {name: players[i].name, pokemon: players[i].pokemon}
-        let opponent = {name: players[Number(!i)].name, pokemon: players[Number(!i)].pokemon}
-        players[i].socket.emit('started', {
-            you: you,
-            opponent: opponent,
-            turn: i === config.turn ? 'you' : 'opponent',
+    for(const [index, player] of players.entries()){
+        const {socket, ...you} = player; 
+        const {socket: _, opponent} = players.find(player => player.socket.id !== socket);
+        socket.emit('started',{
+            you,
+            opponent,
+            turn: index === config.turn ? 'you' : 'opponent',
         });
     }
 };
 
 export const terminateGame = (socket, players) => {
     console.log('Game terminating...');
-    for(let i=0;i<players.length;i++){
-        if(players[i].socket.id === socket.id){
-            players.splice(i,1)
-            console.log('');
-        }else{
-            players[i].pokemon = null;
-            players[i].socket.emit('terminated');
-        }
+    const index = players.findIndex(player=> player.socket.id === socket.id);
+    if(index != -1){
+        players.splice(index,1)
+        console.log('');
+    }
+    for(const player of players){
+        player.pokemon = null;
+        socket.emit('terminated');
     }
 };
 
