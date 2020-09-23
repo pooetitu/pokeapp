62c6ff97a01298d64047e8dd7c31150e1b6464dd
pooetitu <pooetitu@gmail.com>
2020-09-23 10:37:28 +0200

    Checking if more than 2 players are trying to join

--- api/index.js
+++ api/index.js
@@ -1,6 +1,10 @@
 import http from 'http';
 import socketIO from 'socket.io'
 
+
+const players = [];
+const config = { turn: 0 };
+
 const server = http.createServer((req, res) => {
     res.write('Hello world');
     res.end();
@@ -17,14 +21,23 @@ const io = socketIO(server, {
 
 
 io.on('connection', socket => {
-    let name = socket.handshake.query.name;
-    let number = Math.random() * Math.floor(99999);
-    number = Math.floor(number);
-    name += '-' + number;
-    console.log(name + ' is connected');
     socket.emit('connected', 'test emit');
-
+    addNewPlayer(socket)
     socket.on('disconnect', () => {
         console.log('someone disconnected');
     });
 });
+
+function addNewPlayer(socket){
+    if(players.length < 2){
+        let name = socket.handshake.query.name;
+        let number = Math.floor(Math.random() * Math.floor(99999));
+        name += '-' + number;
+        console.log(name + ' is connected');
+        players.push(name);
+    }
+    else{
+        socket.emit('Too many players connected');
+        socket.disconnect();
+    }
+}
